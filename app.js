0328e1c3ff35f2e251b4c5199e17c204396af963
Rossavitz <rcummings04@yahoo.com>
2024-03-05 09:11:26 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let PI = 3.14;
+PI = 42;
+
+// What is the difference between ***var*** and ***let***?
+// var lets you redeclare but let does not. let is block scoped.
+
+// What is the difference between **var** and ***const***?
+//you cannot reassign or redeclare with const
+
+// What is the difference between **let** and **const**?
+//you can reassign with let
+
+// What is hoisting?
+//hoising is how JS reads variables names. var is read before its declared, this includes functions.
